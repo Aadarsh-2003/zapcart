a66ca952f333e9136338056f6c2c31f85a2707f6
Aadarsh-2003 <avi.sharma.2853@gmail.com>
2025-05-26 18:05:35 +0530

    Created register, login, is-auth and logout APIs for user and authentication middlewares for user and seller

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
 import connectDB from './configs/db.js';
-import'dotenv/config';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -20,6 +21,8 @@ app.use(cors({origin: allowedOrigins, credentials : true}));
 app.get("/" , (req,res)=>{
     res.send("API is working");
 });
+
+app.use('/api/user', userRouter);
 
 app.listen(port, ()=>{
     console.log(`App is running on http://localhost: ${port}`);
