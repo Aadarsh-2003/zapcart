6f19f8463e9852df12624def0510a7eba512473d
Aadarsh-2003 <avi.sharma.2853@gmail.com>
2025-06-03 11:55:56 +0530

    Integrated Stripe for payment option

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authUser from "../middlewares/authUser.js";
-import { getAllOrders, getUserOrders, placeOrderCOD } from '../controllers/orderController.js';
+import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe } from '../controllers/orderController.js';
 import authSeller from '../middlewares/authSeller.js';
 
 
@@ -9,6 +9,7 @@ const orderRouter = express.Router();
 orderRouter.post('/cod' , authUser , placeOrderCOD);
 orderRouter.post('/user' , authUser , getUserOrders);
 orderRouter.post('/seller' , authSeller , getAllOrders);
+orderRouter.post('/stripe' , authUser , placeOrderStripe);
 
 
 
